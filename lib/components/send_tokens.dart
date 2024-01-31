d27d6a3caceedd00bec6c11db0fe75f86802c36f
SharmaNityam <142774674+SharmaNityam@users.noreply.github.com>
2024-01-31 19:53:14 +0530

    Add files via upload

---
+++ lib/components/send_tokens.dart
@@ -0,0 +1,111 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:web3dart/web3dart.dart';
+
+class SendTokensPage extends StatelessWidget {
+  final String privateKey;
+  final TextEditingController recipientController = TextEditingController();
+  final TextEditingController amountController = TextEditingController();
+
+  SendTokensPage({Key? key, required this.privateKey}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Send Tokens'),
+        backgroundColor: Colors.transparent,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(170, 0, 191, 255), // Blue color
+              Color.fromARGB(255, 144, 238, 144), // Lighter sea green color
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: recipientController,
+                      decoration: const InputDecoration(
+                        labelText: 'Recipient Address',
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextField(
+                      controller: amountController,
+                      decoration: const InputDecoration(
+                        labelText: 'Amount',
+                      ),
+                      keyboardType:
+                          const TextInputType.numberWithOptions(decimal: true),
+                    ),
+                    const SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        String recipient = recipientController.text;
+                        double amount = double.parse(amountController.text);
+                        BigInt bigIntValue =
+                            BigInt.from(amount * pow(10, 18));
+                        print(bigIntValue);
+                        EtherAmount ethAmount = EtherAmount.fromBigInt(
+                            EtherUnit.wei, bigIntValue);
+                        print(ethAmount);
+                        // Convert the amount to EtherAmount
+                        sendTransaction(recipient, ethAmount);
+                      },
+                      child: const Text('Send'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void sendTransaction(String receiver, EtherAmount txValue) async {
+    var apiUrl = "Your RPC Url"; // Replace with your API
+    // Replace with your API
+    var httpClient = http.Client();
+    var ethClient = Web3Client(apiUrl, httpClient);
+
+    EthPrivateKey credentials = EthPrivateKey.fromHex('0x' + privateKey);
+
+    EtherAmount etherAmount = await ethClient.getBalance(credentials.address);
+    EtherAmount gasPrice = await ethClient.getGasPrice();
+
+    print(etherAmount);
+
+    await ethClient.sendTransaction(
+      credentials,
+      Transaction(
+        to: EthereumAddress.fromHex(receiver),
+        gasPrice: gasPrice,
+        maxGas: 100000,
+        value: txValue,
+      ),
+      chainId: 11155111,
+    );
+  }
+}
